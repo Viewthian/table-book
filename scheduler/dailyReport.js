131f0a1a9c3ca7d74b1c6d3fca3c017d3d02f976
Bond-z <piedmontcnx@gmail.com>
2026-03-08 17:06:18 +0700

    add resend for email sender

--- scheduler/dailyReport.js
+++ scheduler/dailyReport.js
@@ -17,7 +17,7 @@ const sendEmailWithAttachment = require("../utils/sendEmailResend");
 console.log("📅 Scheduler loaded");
 
 cron.schedule(
-  "53 10 * * *",
+  "10 17 * * *",
   async () => {
     try {
       console.log("⏰ Running daily report...");
