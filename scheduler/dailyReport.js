09b9c4e44669fdaa676d4bebe19863a0528ba0be
Bond-z <piedmontcnx@gmail.com>
2026-03-31 17:51:56 +0700

    test cron job email

--- scheduler/dailyReport.js
+++ scheduler/dailyReport.js
@@ -17,7 +17,7 @@ const sendEmailWithAttachment = require("../utils/sendEmailResend");
 console.log("📅 Scheduler loaded");
 
 cron.schedule(
-  "16 16 * * *",
+  "22 22 * * *",
   async () => {
     try {
       console.log("⏰ Running daily report...");
