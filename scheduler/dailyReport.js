8c6183e467a539c073723905be62f8a23108c2ad
Bond-z <piedmontcnx@gmail.com>
2026-04-01 11:17:16 +0700

    change cron job back to 15:15PM

--- scheduler/dailyReport.js
+++ scheduler/dailyReport.js
@@ -17,7 +17,7 @@ const sendEmailWithAttachment = require("../utils/sendEmailResend");
 console.log("📅 Scheduler loaded");
 
 cron.schedule(
-  "22 22 * * *",
+  "15 15 * * *",
   async () => {
     try {
       console.log("⏰ Running daily report...");
